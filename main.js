023436f406b3e5fa4e9db4cd1e2bd3d516498f40
Tomasz Drwięga <tomasz@ethcore.io>
2016-10-15 19:34:41 +0200

    Separating Model and View [Object]

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
-// 2/ We can also use DOM API to create the elements and modify their properties
+//6/ Extract data to separate object.
+var activity = {
+  id: 7,
+  alt: 'Swimmer',
+  name: 'Swimming',
+  timeSpent: 60
+};
+
 var $activity = document.createElement('div');
 $activity.className = 'activity';
 
-// 6/ Creating an image (note classList.add)
 var $img = document.createElement('img');
 $img.classList.add('activity__img');
 $img.width = 250;
 $img.height = 250;
-$img.alt = 'Swimmer';
-$img.src = 'https://xplatform.org/ext/lorempixel/250/250/sports/7/';
+// 2/ We're replacing hardcoded values with references to object.
+$img.alt = activity.alt;
+$img.src = 'https://xplatform.org/ext/lorempixel/250/250/sports/' + activity.id + '/';
 
-// 3/ A title
-var $title = document.createElement('h3');
-$title.classList.add('activity__name');
-$title.textContent = 'Swimming';
+var $name = document.createElement('h3');
+$name.classList.add('activity__name');
+$name.textContent = activity.name;
 
-// 3/ Time Spent
 var $time = document.createElement('p');
 $time.classList.add('activity__description');
-$time.innerHTML = 'Time spent: <strong>60 min</strong>';
+$time.innerHTML = 'Time spent: <strong>' + activity.timeSpent + ' min</strong>';
 
-// 3/ Button
 var $button = document.createElement('button');
 $button.classList.add('activity__button--paused');
 $button.innerHTML = '&#9654; Start';
 
-// 4/ Append all elements to the parent
 $activity.appendChild($img);
-$activity.appendChild($title);
+$activity.appendChild($name);
 $activity.appendChild($time);
 $activity.appendChild($button);
 
-// 2/ And now append to existing element in DOM to render new activity.
 var $activities = document.querySelector('.activities');
 $activities.appendChild($activity);
