907fd44fc946994bff8e696ae433196a372a1b0c
Tomasz Drwięga <tomasz@ethcore.io>
2016-10-16 23:21:12 +0200

    Activities in React

--- src/index.js
+++ src/index.js
@@ -1,41 +1,15 @@
 import 'normalize.css';
+//-- For local development remove !
+import './styles.css!';
 
-// Import React and react-dom
 import React from 'react';
 import { render } from 'react-dom';
 
-//2/ Component needs to extend React.Component
-class App extends React.Component{
-  //3/ Internal state of the component
-  state = {
-    now: 0
-  };
+import Activities from './components/Activities/Activities';
+import Model from './Model';
 
-  //8/ When component is displayed - start the timer.
-  componentWillMount () {
-    setInterval(() => {
-      //3/ We only need to update the state - React will do everything else for us.
-      this.setState({
-        now: new Date().getTime()
-      });
-    }, 500);
-  }
-
-  //10/ Rendering of the component
-  render () {
-    const { now } = this.state;
-    return (
-      <div>
-        <h1>Hello React!</h1>
-        <input type="text" placeholder="some input" />
-        <p>Now: {now}</p>
-      </div>
-    );
-  }
-}
-
-// Just render the app
-render(<App />, document.body);
-
-
-
+//4/ Bootstraping React application, we're passing data to the component
+render(
+  <Activities activities={new Model().getActivities()} />,
+  document.body.querySelector('div')
+);
