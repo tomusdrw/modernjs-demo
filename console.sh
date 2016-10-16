d1902b54c6b023e86aa5cf11262143cbda2081ac
Tomasz Drwięga <tomasz@ethcore.io>
2016-10-16 22:43:46 +0200

    React Way

--- console.sh
+++ console.sh
@@ -1,7 +1,4 @@
 $ cd /path/to/your/project
 
-# Let's install create-react-app (note the development dependency)
-$ npm install react-scripts --save-dev
-
-# We will also need to add some scripts in `package.json`
-
+# Let's install react and react-dom
+$ npm install react react-dom --save
--- src/index.js
+++ src/index.js
@@ -1,33 +1,41 @@
 import 'normalize.css';
 
-//2/ Creating a new component
-class App {
-  //3/ Initial state of the application
+// Import React and react-dom
+import React from 'react';
+import { render } from 'react-dom';
+
+//2/ Component needs to extend React.Component
+class App extends React.Component{
+  //3/ Internal state of the component
   state = {
     now: 0
   };
 
+  //8/ When component is displayed - start the timer.
+  componentWillMount () {
+    setInterval(() => {
+      //3/ We only need to update the state - React will do everything else for us.
+      this.setState({
+        now: new Date().getTime()
+      });
+    }, 500);
+  }
+
   //10/ Rendering of the component
   render () {
     const { now } = this.state;
-    return `
+    return (
       <div>
         <h1>Hello React!</h1>
         <input type="text" placeholder="some input" />
-        <p>Now: ${now}</p>
+        <p>Now: {now}</p>
       </div>
-    `;
+    );
   }
 }
 
-//7/ We need to re-render every 500ms to get some output
-const app = new App();
-setInterval(() => {
-  // We modify the state
-  app.state.now = new Date().getTime();
-  // And we call render() method
-  document.body.innerHTML = app.render();
-}, 500);
+// Just render the app
+render(<App />, document.body);
 
 
 
