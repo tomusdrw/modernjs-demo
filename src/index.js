fcc7ff2035ff1ff6b9ad1d3fe11dd75b2c824f0a
Tomasz Drwięga <tomasz@ethcore.io>
2016-10-16 22:36:59 +0200

    Minimal component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import 'normalize.css';
-//-- For local development remove !
-import './styles.css!';
 
-import Activities from './components/Activities/Activities';
-import Model from './Model';
+//2/ Creating a new component
+class App {
+  //3/ Initial state of the application
+  state = {
+    now: 0
+  };
 
-const view = new Activities(new Model().getActivities());
-document.body.appendChild(view.render());
+  //10/ Rendering of the component
+  render () {
+    const { now } = this.state;
+    return `
+      <div>
+        <h1>Hello React!</h1>
+        <input type="text" placeholder="some input" />
+        <p>Now: ${now}</p>
+      </div>
+    `;
+  }
+}
+
+//7/ We need to re-render every 500ms to get some output
+const app = new App();
+setInterval(() => {
+  // We modify the state
+  app.state.now = new Date().getTime();
+  // And we call render() method
+  document.body.innerHTML = app.render();
+}, 500);
+
+
+
